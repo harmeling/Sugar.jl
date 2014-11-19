5a9e6004f113e9e88c0d8459787a59f26fdfd6cc
Stefan Harmeling <harmeling@gmail.com>
2014-11-19 10:24:37 +0100

    better handling of small matrices in imagesc

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ To install this package you run
 julia> Pkg.clone("git://github.com/harmeling/Sugar.jl.git/")
 ```
 
-## Functions
+## `imagesc` based on `Images.jl`
 
 To get a quick impression of a matrix it is often good to get a visual
 impression of it.  This is what `imagesc` does.  It converts it to an
@@ -20,4 +20,13 @@ Image that is shown using the excellent `Images.jl` package.  E.g.
 
 ```
 imagesc(randn(100,120))
+```
+
+## Really simple plotting of vectors and matrices with `Gadfly.jl`
+
+```
+plot(v, w)  # plots vector w against vector v
+plot(w)     # plots vector w against [1:length(w)]
+plot(v, A)  # plots columns of A against vector v
+plot(A)     # plots columsn of A against [1:size(A,1)]
 ```
--- src/Sugar.jl
+++ src/Sugar.jl
@@ -1,15 +1,29 @@
 module Sugar
-# author: Stefan Harmeling
-# date  : 2014-11-16
+# author      : Stefan Harmeling
+# date        : 2014-11-16
+# description : convenience function, aka syntactic sugar
 
+using Color, Images, Gadfly, DataFrames
+
+import Gadfly.plot
+export imagesc, plot
+
+## syntactic sugar for Images.jl
 using Color, Images
-
-export imagesc
-
 scale0_1(A::Array) = (A - minimum(A))./(maximum(A)-minimum(A))
 scale1_255(A::Array) = 254 * scale0_1(A) + 1
 cmblues   = colormap("Blues", 255)
 cmoranges = colormap("Oranges", 255)
-imagesc(x, cm=cmblues) = ImageCmap(iround(scale1_255(x')), cm)
+imagesc(x; cm=cmblues, minsize=100, zoom=iceil(minsize ./ [size(x)...])) = ImageCmap(kron(iround(scale1_255(x')), ones(zoom[2],zoom[1])), cm)
+
+##  syntactic sugar for Gadfly.jl
+#   plot(v, w)  plots vector w against vector v
+#   plot(w)     plots vector w against [1:length(w)]
+#   plot(v, A)  plots columns of A against vector v
+#   plot(A)     plots columsn of A against [1:size(A,1)]
+plot(v::Vector, w::Vector) = plot(x=v, y=w)
+plot(w::Vector) = plot([1:length(w)], w)
+plot(v::Vector, A::Matrix) = plot(vcat([DataFrame(x=v, y=A[:,i], color="$i")  for i in 1:size(A,2)]...), x="x", y="y", color="color", Geom.line)
+plot(A::Matrix) = plot([1:size(A,1)], A)
 
 end # module
