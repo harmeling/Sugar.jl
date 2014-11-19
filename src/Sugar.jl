d46bc4083c01b5b252cc80ba42470f2e8fcc61d5
Stefan Harmeling <harmeling@gmail.com>
2014-11-19 10:26:48 +0100

    swapping x and y axis in imagesc

--- src/Sugar.jl
+++ src/Sugar.jl
@@ -14,7 +14,7 @@ scale0_1(A::Array) = (A - minimum(A))./(maximum(A)-minimum(A))
 scale1_255(A::Array) = 254 * scale0_1(A) + 1
 cmblues   = colormap("Blues", 255)
 cmoranges = colormap("Oranges", 255)
-imagesc(x; cm=cmblues, minsize=100, zoom=iceil(minsize ./ [size(x)...])) = ImageCmap(kron(iround(scale1_255(x')), ones(zoom[2],zoom[1])), cm)
+imagesc(x; cm=cmblues, minsize=100, zoom=iceil(minsize ./ [size(x)...])) = ImageCmap(kron(iround(scale1_255(x)), ones(zoom[1],zoom[2])), cm)
 
 ##  syntactic sugar for Gadfly.jl
 #   plot(v, w)  plots vector w against vector v
